e0f5d743abf2bf2710592e5e594b53f2cdd6bf05
Tomasz Siudak <tomaszsiudak00@gmail.com>
2021-10-10 13:03:49 +0200

    RepeaterApplication

--- src/test/java/com/quiz/quizapplication/exercises/TestSuiteDataAddExercises.java
+++ src/test/java/com/quiz/quizapplication/exercises/TestSuiteDataAddExercises.java
@@ -11,25 +11,25 @@ import java.sql.Statement;
 
 public class TestSuiteDataAddExercises {
 
-    DataAddExercisesGroup dataAddExercisesGroup;
-    GroupExercises groupExercises = new GroupExercises("Math");
-
-    String sqlQueryAddGroup = "INSERT INTO GROUP_EXERCISES(NAME, ARCHIVED) " +
-            "VALUES ('" + groupExercises.getGroupName() + "','" + groupExercises.isArchived() + "')";
-
-    @BeforeEach
-    @AfterEach
-    public void cleanUp() throws SQLException {
-        String sqlDel = "DELETE FROM EXERCISES;\nDELETE FROM GROUP_EXERCISES;";
-        Statement statement = ConnectToDb.getInstance().getConn().createStatement();
-        statement.executeUpdate(sqlDel);
-        statement.close();
-    }
-
-    @BeforeEach
-    public void initialize() throws SQLException {
-        dataAddExercisesGroup = new DataAddExercisesGroup();
-    }
+//    DataAddExercisesGroup dataAddExercisesGroup;
+//    GroupExercises groupExercises = new GroupExercises("Math");
+//
+//    String sqlQueryAddGroup = "INSERT INTO GROUP_EXERCISES(NAME, ARCHIVED) " +
+//            "VALUES ('" + groupExercises.getGroupName() + "','" + groupExercises.isArchived() + "')";
+//
+//    @BeforeEach
+//    @AfterEach
+//    public void cleanUp() throws SQLException {
+//        String sqlDel = "DELETE FROM EXERCISES;\nDELETE FROM GROUP_EXERCISES;";
+//        Statement statement = ConnectToDb.getInstance().getConn().createStatement();
+//        statement.executeUpdate(sqlDel);
+//        statement.close();
+//    }
+//
+//    @BeforeEach
+//    public void initialize() throws SQLException {
+//        dataAddExercisesGroup = new DataAddExercisesGroup();
+//    }
 
 //    @Test
 //    void test_addExercisesGroup() {
